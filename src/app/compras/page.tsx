28f8fd7e2044abbb9001f754b47fba40ad917efb
Alice Santos <86413853+Lice-Santos@users.noreply.github.com>
2024-10-29 09:44:40 -0300

    first commit

---
+++ src/app/compras/page.tsx
@@ -0,0 +1,7 @@
+export default function Compras(){
+    return(
+        <main>
+            <h1 className="text-center text-4xl text-indigo-600 font-bold">Página de Compras</h1>
+        </main>
+    )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return(
+    <div className="flex justify-center p-10">
+      <h1 className="text-center text-4xl text-indigo-600 font-bold">home</h1>
+    </div>
+  )
+}
